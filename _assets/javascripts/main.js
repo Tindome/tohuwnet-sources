2357c314b4cda126d2cbed82058299b84bd027a0
Ron Scott-Adams <ron@tohuw.net>
2016-05-09 02:53:26 -0400

    Fixed menu, style cleanups, index page import
    Now the index page works more like the old version of this site.
    TODO: Find out why the text for the 2nd article displays below the body
    of the first…

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -40,3 +40,10 @@ $('#search-results .close-button').click(function() {
     $('#search-results').removeClass('active');
     $('#search_box').val('');
 });
+
+$(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
